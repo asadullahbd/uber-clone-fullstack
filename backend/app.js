845ea6bf90833c49b3576500441fb0eca0dc68c0
asadullahbd <asadullah70209536@gmail.com>
2025-01-28 17:24:36 +0600

    added vercel.json

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: `${process.env.FRONTEND_LINK}`, 
+    origin: `${process.env.FRONTEND_LINK}` || "http://localhost:5173", 
     credentials: true // Allow cookies to be sent
   }));
 app.use(cookieParser());
