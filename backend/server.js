6dd39d2f48e2998f335d4b8f5995c2ea901466fd
asadullahbd <asadullah70209536@gmail.com>
2025-01-30 08:54:25 +0600

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
 import http from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
