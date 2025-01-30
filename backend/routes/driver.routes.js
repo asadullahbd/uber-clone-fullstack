186220765b20bab1b9e956f69e06bc4c0076ecae
asadullahbd <asadullah70209536@gmail.com>
2025-01-30 09:03:02 +0600

    check route

--- backend/routes/driver.routes.js
+++ backend/routes/driver.routes.js
@@ -9,7 +9,7 @@ router.post('/login',loginDriverController)
 router.get('/profile',authDriverMiddleware,profileDriverController)
 
 router.get('/check',(req,res)=>{
-    res.send('Driver route is working fine')
+    res.send('Driver route is working fine check check')
 })
 
 export default router
